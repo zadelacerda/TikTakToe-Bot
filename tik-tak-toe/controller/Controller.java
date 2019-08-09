71a37dac8617fed9a3a38059575a6459446184b9
Benjamin Larkin <Ben>
2019-08-09 13:24:35 -0700

    skeleton code added, need to implement packages before compiling

--- tik-tak-toe/controller/Controller.java
+++ tik-tak-toe/controller/Controller.java
@@ -1,13 +1,50 @@
-import java.util.*;
+// import java.util.*;
+// import model.*;
+// import view.*;
 
-public class Controller{
-    private static TikTakToe game;
-    private static View view;
-    private static Controller controller = new Controller();
+// public class Controller {
+//     private static TikTakToe game;
+//     private static View view;
+//     private static Controller controller = new Controller();
 
-    private Controller(){
-        game = TikTakToe.newGame(); // lazy initialization/singleton
-        view = new View();
-        // view.startGame();
-    }
-}
+//     private Controller() {
+//         game = TikTakToe.newGame(); // lazy initialization/singleton
+//         view = new View();
+//         // view.startGame();
+//     }
+
+//     public static Controller getController() {
+//         return controller;
+//     }
+
+//     /* view related functions */
+
+//     public void startGame() {
+//         // game.startGame();
+//     }
+
+//     public static void gameButtonPressed() {
+//         // update model/gamestate
+//     }
+
+//     public static void exitButtonPressed() {
+//         // close program
+//     }
+
+//     /* model related functions */
+
+//     // updates view to display winning player
+//     public static void gameOver(Player player) {
+
+//     }
+
+//     // updates scoreboard with current game count
+//     public static void updateScoreBoard(int[] score) {
+
+//     }
+
+//     public static void updateCurrPlayer(Player player) {
+
+//     }
+
+// }
